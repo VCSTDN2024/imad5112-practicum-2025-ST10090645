e70d62ba403c6e148a45acb4548cc0fcbe1b4e76
Dylan Mbamba <ST10090645@vcconnect.edu.za>
2025-06-19 08:51:35 +0200

    First Version. UI for first screen implemented.

---
+++ app/src/main/java/com/example/imad5112practicummusicplaylistapp_st10090645/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.imad5112practicummusicplaylistapp_st10090645
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+}
